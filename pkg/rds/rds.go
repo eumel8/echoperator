e2ac0227d9b9aee1953af710b462bc9b907458f2
Frank Kloeker <f.kloeker@telekom.de>
2022-04-07 10:19:24 +0200

    add rds package

---
+++ pkg/rds/rds.go
@@ -0,0 +1,9 @@
+package rds
+
+const (
+	GroupName = "mcsps.de"
+
+	V1alpha1 = "v1alpha1"
+
+	RdsKind          string = "Rds"
+)
---
+++ pkg/rds/v1alpha1/register.go
@@ -0,0 +1,34 @@
+package v1alpha1
+
+import (
+	"github.com/mmontes11/echoperator/pkg/echo"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	SchemeGroupVersion = schema.GroupVersion{
+		Group:   echo.GroupName,
+		Version: echo.V1alpha1,
+	}
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme   = SchemeBuilder.AddToScheme
+)
+
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&Echo{},
+		&EchoList{},
+		&ScheduledEcho{},
+		&ScheduledEchoList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+
+	return nil
+}
